c0f610e5b86208eecc2fec6698a164aa0c18e946
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2021-05-02 20:06:29 -0300

    Small changes on the button and add a new circle on screen of game of mouse

--- JS/tracker-script.js
+++ JS/tracker-script.js
@@ -1,32 +1,58 @@
 const AREA = document.body;
-const CIRCLE = document.querySelector('.circle');
+const RIGHTBOTTOM = document.querySelector("#right-bottom");
+const LEFTTOP = document.querySelector("#left-top");
 
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
 
 function mouseCoordinates(e) {
-    // Captura o objeto de evento (movimento do mouse).
-    // Obtém a coordenada X (distância da borda esquerda da janela de visualização) através da propriedade clientX.
-    // Pega a largura total da janela, subtrai a coordenada atual e a largura do círculo.
-    // Faz o mesmo para a coordenada Y (distância da borda superior da janela de visualização).
-    var horizontalPosition = windowWidth - e.clientX - 26;
-    var verticalPosition= windowHeight - e.clientY - 26;
+  // Captura o objeto de evento (movimento do mouse).
+  // Obtém a coordenada X (distância da borda esquerda da janela de visualização) através da propriedade clientX.
+  // Pega a largura total da janela, subtrai a coordenada atual e a largura do círculo.
+  // Faz o mesmo para a coordenada Y (distância da borda superior da janela de visualização).
+  var horizontalPosition = windowWidth - e.clientX - 26;
+  var verticalPosition = windowHeight - e.clientY - 26;
 
-    // Define a posição horizontal e vertical.
-    CIRCLE.style.left = horizontalPosition + 'px';
-    CIRCLE.style.top = verticalPosition + 'px';
+  // Define a posição horizontal e vertical.
+  RIGHTBOTTOM.style.right = horizontalPosition + "px";
+  RIGHTBOTTOM.style.top = verticalPosition + "px";
+  LEFTTOP.style.left = horizontalPosition + "px";
+  LEFTTOP.style.top = verticalPosition + "px";
 }
 
-function changeColorOnTouch() {
-    CIRCLE.style.backgroundColor = "green";
-    CIRCLE.style.borderColor = "green";
+function changeColorOnTouchLeft() {
+  LEFTTOP.style.backgroundColor = "green";
+  LEFTTOP.style.borderColor = "green";
+}
+
+function changeColorOnTouchRight() {
+  RIGHTBOTTOM.style.backgroundColor = "green";
+  RIGHTBOTTOM.style.borderColor = "green";
 }
 
 // Quando o mouse se move, executa a função mouseCoordinates.
-AREA.addEventListener('mousemove', mouseCoordinates, false);
+AREA.addEventListener("mousemove", mouseCoordinates, false);
 
 // Quando o mouse toca o círculo, executa a função changeColorOnTouch.
-CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+LEFTTOP.addEventListener("mouseenter", changeColorOnTouchLeft, false);
+RIGHTBOTTOM.addEventListener(
+  "mouseenter",
+  changeColorOnTouchRight,
+  false
+);
 
 // Quando o mouse sai do círculo, remove estilos embutidos com uma função anônima.
-CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+LEFTTOP.addEventListener(
+  "mouseleave",
+  function () {
+    LEFTTOP.removeAttribute("style");
+  },
+  false
+);
+RIGHTBOTTOM.addEventListener(
+  "mouseleave",
+  function () {
+    RIGHTBOTTOM.removeAttribute("style");
+  },
+  false
+);
